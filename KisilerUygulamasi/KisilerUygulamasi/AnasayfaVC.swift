17c4d1355a1b3c7dea3becfc787131da8df0876d
MaSheN <muhalishen@gmail.com>
2022-08-10 20:02:38 +0300

    delete actions added

--- KisilerUygulamasi/KisilerUygulamasi/AnasayfaVC.swift
+++ KisilerUygulamasi/KisilerUygulamasi/AnasayfaVC.swift
@@ -11,18 +11,28 @@ class AnasayfaVC: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
     
+    @IBOutlet weak var kisilerTableView: UITableView!
+    
+    var kisilerListe = [Kisiler]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        kisilerTableView.delegate = self
+        kisilerTableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        let k1 = Kisiler(kisi_id: 1, kisi_ad: "ahmet", kisi_tel: "11111")
+        let k2 = Kisiler(kisi_id: 2, kisi_ad: "mehmet", kisi_tel: "22222")
+        let k3 = Kisiler(kisi_id: 3, kisi_ad: "zeynep", kisi_tel: "33333")
+        
+        kisilerListe.append(k1)
+        kisilerListe.append(k2)
+        kisilerListe.append(k3)
     }
 
 
-    @IBAction func buttonTikla(_ sender: Any) {
-        let kisi = Kisiler(kisi_id: 1, kisi_ad: "Ahmet", kisi_tel: "123123")
-        performSegue(withIdentifier: "toDetay", sender: kisi)
-    }
+  
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDetay" {
@@ -38,6 +48,51 @@ class AnasayfaVC: UIViewController {
 extension AnasayfaVC: UISearchBarDelegate{
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             print("Arama Sonucu: \(searchText)")
+    }
+}
+
+extension AnasayfaVC: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kisilerListe.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListe[indexPath.row]
+        let hucre = tableView.dequeueReusableCell(withIdentifier: "kisilerHucre", for: indexPath) as! TableViewHucre
+        
+        hucre.kisiBilgiLabel.text = "\(kisi.kisi_ad!) - \(kisi.kisi_tel!)"
+        return hucre
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let kisi = kisilerListe[indexPath.row]
+        performSegue(withIdentifier: "toDetay", sender: kisi)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let silAction = UIContextualAction(style: .destructive, title: "Sil"){(contextualAction,view,bool) in
+            let kisi = self.kisilerListe[indexPath.row]
+            
+            let alert = UIAlertController(title: "Silme Islemi", message: "\(kisi.kisi_ad!) silinsin mi?", preferredStyle: .alert)
+            
+            let iptalAction = UIAlertAction(title: "Iptal", style: .cancel) {
+                action in }
+            
+            let evetAction = UIAlertAction(title: "Evet", style: .destructive){
+                action in
+                print("\(kisi.kisi_ad!) silindi")
+                //self.kisilerListe.remove(at: indexPath.row)
+                //tableView.reloadData()
+            }
+            
+            alert.addAction(evetAction)
+            
+            alert.addAction(iptalAction)
+            self.present(alert, animated: true)
+            
+            
+        }
+        return UISwipeActionsConfiguration(actions: [silAction])
     }
 }
 
---
+++ KisilerUygulamasi/KisilerUygulamasi/TableViewHucre.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewHucre.swift
+//  KisilerUygulamasi
+//
+//  Created by MaSheN on 10.08.2022.
+//
+
+import UIKit
+
+class TableViewHucre: UITableViewCell {
+
+    @IBOutlet weak var kisiBilgiLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
